d008d36885809bb3f896349a6bc214a397693c6e
Alexandre ANDREI <aandrei-ext@pcis.lvmh-pc.com>
2016-10-06 17:47:10 +0200

    Change variables + mappings.

--- src/main/java/fr/ippon/aandrei/test/service/RestController.java
+++ src/main/java/fr/ippon/aandrei/test/service/RestController.java
@@ -1,16 +1,20 @@
 package fr.ippon.aandrei.test.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RestController {
 
+    @Value("${hello.msg:Hello}") private String helloMsg;
+
     @ResponseBody
-    @RequestMapping("/user")
-    public String getUser() {
-        return "hello";
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        return helloMsg;
     }
 
 }
